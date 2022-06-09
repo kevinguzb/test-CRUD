b5ced2eef608f5fabda852afeff9301800a83882
kevinguzb <80874952+kevinguzb@users.noreply.github.com>
2022-06-09 12:51:06 -0500

    Add files via upload

---
+++ base_de_datos.php
@@ -0,0 +1,13 @@
+<?php
+$contrase = "kevin11";
+$usuario = "postgres";
+$nombreBaseDeDatos = "mascotas";
+$rutaServidor = "localhost";
+$puerto = "5432";
+try {
+    $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contrase);
+    $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo "Ocurrió un error con la base de datos: " . $e->getMessage();
+}
+?>
---
+++ encabezado.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="es">
+<!--
+CRUD con PostgreSQL y PHP
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>CRUD</title>
+    <!-- Cargar el CSS de Boostrap-->
+    <link href="./bootstrap.min.css" rel="stylesheet">
+    <!-- Cargar estilos propios -->
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <!-- Definición del menú -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <a class="navbar-brand" target="_blank">
+      CRUD
+        </a>
+        <div class="collapse navbar-collapse" id="miNavbar">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="./listar.php">Ver Registros</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./formulario.php">Agregar</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Termina la definición del menú -->
+    <main role="main" class="container">
